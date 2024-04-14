568433b1c60a322d73f762440ad078305ff4bfc0
Anej <majnik2001@gmail.com>
2024-04-14 11:42:12 +0200

    Uredil zapakiraj_27119.sh in 27119_deploy.yml

---
+++ zapakiraj_27119.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Prijava v docker
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+
+echo "date=$(date +'%Y-%m-%d--%M-%S')" >> "$GITHUB_ENV"
+
+
+# izgradnja docker slike
+docker build . --file Dockerfile --tag $REPONAME:$date
+
+# Potisnite sliko na Docker Hub
+docker push $REPONAME:$date
