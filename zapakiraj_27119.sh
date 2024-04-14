7ad9147f158aa4d9d1be683574d31b0777ead97a
Anej <majnik2001@gmail.com>
2024-04-14 11:47:15 +0200

    Dodal generacijo date

--- zapakiraj_27119.sh
+++ zapakiraj_27119.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# generacija date
+date=$(date +'%Y-%m-%d--%M-%S')
+
+# print date
 echo "Date: $date"
 
 # Prijava v docker
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
-echo "date=$(date +'%Y-%m-%d--%M-%S')" >> "$GITHUB_ENV"
-
+echo "date=$date" >> "$GITHUB_ENV"
 
 # izgradnja docker slike
 docker build . --file Dockerfile --tag $REPONAME:$date
 
-# Potisnite sliko na Docker Hub
+# Push slike na Docker Hub
 docker push $REPONAME:$date
