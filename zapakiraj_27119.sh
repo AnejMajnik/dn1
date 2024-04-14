ef110fcf6a25767f8549496496212b5dc967ae7a
Anej <majnik2001@gmail.com>
2024-04-14 12:26:08 +0200

    Dodal TAG

--- zapakiraj_27119.sh
+++ zapakiraj_27119.sh
@@ -9,7 +9,7 @@ echo "Date: $date"
 # Prijava v docker
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
-echo "date=$date" >> "$GITHUB_ENV"
+echo "TAG=$date" >> "$GITHUB_ENV"
 
 # izgradnja docker slike
 docker build . --file Dockerfile --tag $REPONAME:$date
--- zazeni_27119.sh
+++ zazeni_27119.sh
@@ -3,6 +3,6 @@
 # Prijava v docker
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
-docker pull $REPONAME:date
+docker pull $REPONAME:$TAG
 
-docker run $REPONAME:date
+docker run $REPONAME:$TAG
